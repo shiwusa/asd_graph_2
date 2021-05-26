b0cb5213646cb2aa7b71db781f10863a70c2872c
shiwusa <71709272+shiwusa@users.noreply.github.com>
2021-05-26 15:35:24 +0300

    try catch

--- main 2.c
+++ main 2.c
@@ -591,13 +591,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT messg,
                 }
             printf("\nto draw condenc graph\n");
             int x = -1;
-            while ( 1 )
-            {
-                printf("write number > 0\n");
-                scanf("%i", &x);
-                if ( x > 0 ) break;
-            }
-            drawer(hdc, pretty, on);
+            //while ( 1 )
+            //{
+                //printf("write number > 0\n");
+                //scanf("%i", &x);
+                //if ( x > 0 ) break;
+            //}
+            //drawer(hdc, pretty, on);
 /*
             for ( int i = 0; i < 4; i++ ) {
                 for ( int j = 0; j < 8; j++ ) {
